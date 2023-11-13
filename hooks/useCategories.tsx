f0a8661644ddaf8245047dc266f983bfe1f56d2b
fodhil-ben <benhibafodhil@gmail.com>
2023-11-13 21:02:58 +0100

    test

--- hooks/useCategories.tsx
+++ hooks/useCategories.tsx
@@ -5,7 +5,7 @@ import { useContext } from "react"
 function useCategories() {
     const context: any = useContext(CategoriesContext)
     if (context) {
-        console.log(context)
+        console.log(context);
         return { ...context }
     } else {
         throw new Error('Categories context must be used inside CategoriesProvider')
